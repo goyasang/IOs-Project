3e51257d07be6b9550e657f41f4b82fcf2a4018e
Xcode User <xcode@Fasts-MacBook-Pro-2.local>
2016-10-31 10:13:10 -0400

    Initial Commit

---
+++ Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Project
+//
+//  Created by Xcode User on 2016-10-31.
+//  Copyright © 2016 Xcode Userproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Project/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Project
+//
+//  Created by Xcode User on 2016-10-31.
+//  Copyright © 2016 Xcode Userproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
